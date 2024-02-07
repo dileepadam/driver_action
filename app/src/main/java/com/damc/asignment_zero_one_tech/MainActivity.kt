9a8b22feebe51ab68a6f4fada025ddf8ffb5b4cd
Dileepa Dampriya <ddampriya1@gmail.com>
2024-02-07 20:17:15 +0530

    Initial commit

---
+++ app/src/main/java/com/damc/asignment_zero_one_tech/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.damc.asignment_zero_one_tech
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
